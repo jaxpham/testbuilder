8d638157e33dad3b39ec4238e1ea26c3556e82b2
Jackson Pham <jackson.pham@gmail.com>
2020-10-08 15:40:37 -0700

    "Incomplete Discover and Mastro Test"

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -138,10 +138,23 @@ describe('MasterCard', function() {
 describe('Discover', function() {
   // Tests without a function will be marked as "pending" and not run
   // Implement these tests (and others) and make them pass!
-  it('has a prefix of 6011 and a length of 16');
-  it('has a prefix of 6011 and a length of 19');
+  var should = chai.should();
+
+  it('has a prefix of 6011 and a length of 16', function() {
+    detectNetwork('6011123412341234').should.equal('Discover');
+  });
+
+  it('has a prefix of 6011 and a length of 19', function() {
+    detectNetwork('6011123412341234123').should.equal('Discover');
+  });
+
 });
 
 describe('Maestro', function() {
   // Write full test coverage for the Maestro card
+  var should = chai.should();
+
+  it('has a prefix of 6011 and a length of 16', function() {
+    detectNetwork('6011123412341234').should.equal('Discover');
+  });
 });
